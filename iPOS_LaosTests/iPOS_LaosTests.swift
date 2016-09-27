12367868fca5718fffdd37e8dccf546b46a79794
NEXUS Mac <nexusmac@imac-khxng-nexus.local>
2016-09-27 15:47:39 +0700

    Initial Commit

---
+++ iPOS_LaosTests/iPOS_LaosTests.swift
@@ -0,0 +1,36 @@
+//
+//  iPOS_LaosTests.swift
+//  iPOS_LaosTests
+//
+//  Created by NEXUS Mac on 27/9/59.
+//  Copyright © พ.ศ. 2559 NEXUS Mac. All rights reserved.
+//
+
+import XCTest
+@testable import iPOS_Laos
+
+class iPOS_LaosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
